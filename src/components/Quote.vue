9a4300dee6a503d8438a805ff2d16217ee05d000
Allison Tarr <hello@allisontarr.com>
2017-09-17 12:22:49 -0400

    add getRandomQuote method + have it appear on initial page load

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -5,12 +5,23 @@
 </template>
 
 <script>
+	import {quotes} from '../assets/quotes.json';
+
 	export default {
 		name: 'quote',
 		data: function() {
 			return {
-				quote: "123 testing test",
+				quote: "",
 			}
+		},
+		methods: {
+			getRandomQuote: function() {
+				let index = Math.floor( Math.random()*( quotes.length ) );
+				this.quote = quotes[index];
+			}
+		},
+		mounted: function() {
+			this.getRandomQuote();
 		}
 	}
 </script>
