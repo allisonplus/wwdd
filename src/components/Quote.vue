3335e0fb247c00614bfbc3be34d4f8aa24e4046e
Allison Tarr <hello@allisontarr.com>
2017-09-17 12:36:29 -0400

    bind click event to button to bring in new random quote

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="quote">
 		<p>{{quote}}</p>
-	<button class="button">Wise Words, Please</button>
+	<button @click="getRandomQuote" class="button">Wise Words, Please</button>
 	</div>
 </template>
 
