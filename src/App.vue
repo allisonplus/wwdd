d5e192eb09294f5ffe25f6dcc553c2210be1f286
Allison Tarr <hello@allisontarr.com>
2017-09-18 09:01:17 -0400

    revert to nix banner component

--- src/App.vue
+++ src/App.vue
@@ -13,10 +13,21 @@
 		</div> <!--/.about-drop-->
 
 		<div class="wrapper">
-				<Banner
-					v-bind:msg="msg"
-					v-bind:isActive="isActive"
-				></Banner>
+				<header>
+					<button
+						class="about-button"
+						v-bind:class="{ active: isActive }"
+						v-on:click="isActive = !isActive"
+					>
+						<span class="mobile-menu-bar line-1"></span>
+						<span class="mobile-menu-bar line-2"></span>
+						<span class="mobile-menu-bar line-3"></span>
+					</button>
+					<h1>{{msg}}</h1>
+				<div class="twitter">
+					<a href="https://twitter.com/intent/tweet?text=%22It%27s%20hard%20to%20be%20a%20diamond%20in%20a%20rhinestone%20world.%22%20%20-%20What%20Would%20Dolly%20Do%3F%20-%20whatwoulddollydo.com%20via%20%40allisonplus%20%20%20%20%23wwdd" target="_"><i class="fa fa-twitter"></i></a>
+				</div> <!--/.twitter-->
+				</header>
 
 				<div class="content">
 
@@ -31,7 +42,6 @@
 </template>
 
 <script>
-import Banner from './components/Banner.vue';
 import Quote from './components/Quote.vue';
 
 export default {
@@ -68,7 +78,6 @@ export default {
 		this.getRandomColour();
 	},
 	components: {
-		Banner,
 		Quote
 	}
 }
