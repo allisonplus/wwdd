e8340826e418922d828b7806b4a03ddbbd69b1d2
Allison Tarr <hello@allisontarr.com>
2017-09-17 23:25:17 -0400

    add media query beginnings

--- src/App.vue
+++ src/App.vue
@@ -26,10 +26,9 @@
 					<span class="mobile-menu-bar line-3"></span>
 				</button>
 				<h1>{{msg}}</h1>
-
-				<div class="twitter">
-					<a href="https://twitter.com/intent/tweet?text=%22It%27s%20hard%20to%20be%20a%20diamond%20in%20a%20rhinestone%20world.%22%20%20-%20What%20Would%20Dolly%20Do%3F%20-%20whatwoulddollydo.com%20via%20%40allisonplus%20%20%20%20%23wwdd" target="_"><i class="fa fa-twitter"></i></a>
-				</div> <!--/.twitter-->
+			<div class="twitter">
+				<a href="https://twitter.com/intent/tweet?text=%22It%27s%20hard%20to%20be%20a%20diamond%20in%20a%20rhinestone%20world.%22%20%20-%20What%20Would%20Dolly%20Do%3F%20-%20whatwoulddollydo.com%20via%20%40allisonplus%20%20%20%20%23wwdd" target="_"><i class="fa fa-twitter"></i></a>
+			</div> <!--/.twitter-->
 			</header>
 
 				<div class="content">
