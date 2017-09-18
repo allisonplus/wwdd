80cff22d842151a7ef9accfdfba02c7145275ecf
Allison Tarr <hello@allisontarr.com>
2017-09-18 14:53:55 -0400

    update OG assets, font style src, add screen-reader-text, adjust hover of quote button and font particulars

--- src/App.vue
+++ src/App.vue
@@ -19,13 +19,14 @@
 						v-bind:class="{ active: isActive }"
 						v-on:click="isActive = !isActive"
 					>
+						<p class="screen-reader-text">Menu</p>
 						<span class="mobile-menu-bar line-1"></span>
 						<span class="mobile-menu-bar line-2"></span>
 						<span class="mobile-menu-bar line-3"></span>
 					</button>
 					<h1>{{msg}}</h1>
 				<div class="twitter">
-					<a href="https://twitter.com/intent/tweet?text=%22It%27s%20hard%20to%20be%20a%20diamond%20in%20a%20rhinestone%20world.%22%20%20-%20What%20Would%20Dolly%20Do%3F%20-%20whatwoulddollydo.com%20via%20%40allisonplus%20%20%20%20%23wwdd" target="_"><i class="fa fa-twitter"></i></a>
+					<a href="https://twitter.com/intent/tweet?text=%22It%27s%20hard%20to%20be%20a%20diamond%20in%20a%20rhinestone%20world.%22%20%20-%20What%20Would%20Dolly%20Do%3F%20-%20whatwoulddollydo.com%20via%20%40allisonplus%20%20%20%20%23wwdd" target="_"><span class="screen-reader-text">Share this site via Twitter</span><i class="fa fa-twitter"></i></a>
 				</div> <!--/.twitter-->
 				</header>
 
